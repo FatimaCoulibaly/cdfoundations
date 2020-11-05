e1e683760a7dd5d0bb2ac3991a40e3d44b5a1a04
FatimaCoulibaly <42788249+FatimaCoulibaly@users.noreply.github.com>
2020-11-04 23:05:04 -0500

    Update main.js

--- functions/main.js
+++ functions/main.js
@@ -1,1 +1,23 @@
-ECHO is on.
+// global variables
+let selection = 0
+
+function update() {
+	// local variables
+	let options = document.querySelectorAll('.option')
+	console.log(options)
+	options.forEach(item => item.onclick=function() {
+		selection = item.id
+		console.log(selection)})
+
+}
+
+function myFunction() {
+  document.getElementById("happy").innerHTML = "I am happy!";
+
+    document.getElementById("neutral").innerHTML = "I am neutral!";
+
+  document.getElementById("sad").innerHTML = "I am sad!";
+
+}
+
+update();
