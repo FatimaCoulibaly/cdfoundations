151ec418a738ea6081e88dda7a7b7feddf8d4a95
FatimaCoulibaly <42788249+FatimaCoulibaly@users.noreply.github.com>
2020-11-06 00:02:33 -0500

    Update main.js

--- functions/main.js
+++ functions/main.js
@@ -4,20 +4,17 @@ let selection = 0
 function update() {
 	// local variables
 	let options = document.querySelectorAll('.option')
+	let title = document.querySelector('h1')
 	console.log(options)
 	options.forEach(item => item.onclick=function() {
 		selection = item.id
-		console.log(selection)})
+		console.log(selection)
+		title.innerHTML= "Today I feel " + selection + " because of the weather."
 
-}
-
-function myFunction() {
-  document.getElementById("happy").innerHTML = "I am happy!";
-
-    document.getElementById("neutral").innerHTML = "I am neutral!";
-
-  document.getElementById("sad").innerHTML = "I am sad!";
+	})
 
 }
 
 update();
+
+
